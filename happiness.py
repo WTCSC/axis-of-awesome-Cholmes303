eac327bdc1108441c343c87ce33273b5af70e7a4
cristianh <cj@jasonholmes.net>
2025-03-26 15:08:07 -0600

    Finished editing csv files

--- happiness.py
+++ happiness.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib as plt
 
-drugs = pd.read_csv("DRUGS.csv")
-print(drugs)
+gdp_df = pd.read_csv("gdp_2015.csv")
+print(gdp_df)
+
+happiness_df = pd.read_csv("happiness_2015.csv")
+print(happiness_df)
