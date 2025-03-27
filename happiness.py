a01279a03783a0df74ddd5a1e9c6d228ec7738d1
cholmes303 <cj@holmesholdingsllc.com>
2025-03-27 10:59:16 -0600

    edit csv

--- happiness.py
+++ happiness.py
@@ -1,8 +1,14 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import csv
 
 gdp_df = pd.read_csv("gdp_2015.csv")
 print(gdp_df)
 
 happiness_df = pd.read_csv("happiness_2015.csv")
 print(happiness_df)
+  
+
+
+
+
